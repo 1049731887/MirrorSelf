c54ddec84d5c6a2463a474d34ade5f80d39da25f
SsuJo_ <1049731887@qq.com>
2025-10-18 14:59:05 +0800

    api point test

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,13 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("Hello, World!")
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!\n")
+	})
+
+	app.Listen(":3001")
 }
